ce8cb61e11cc91ccd3b9f1069b78bd6fc4bfb578
gyafdxis-king <1007881011@qq.com>
2022-10-16 18:11:13 +0800

    os

---
+++ x86/source/kernel/include/os_cfg.h
@@ -0,0 +1,5 @@
+#ifndef OS_CFG_H
+#define OS_CFG_H
+
+#define GDT_TABLE_SIZE 256
+#endif
---
+++ x86/source/kernel/init/init.c
@@ -0,0 +1,9 @@
+#include "init.h"
+#include "comm/boot_info.h"
+#include "cpu/cpu.h"
+void kernel_init (boot_info_t * boot_info) {
+    cpu_init ();
+    for (;;) {
+
+    }
+}
